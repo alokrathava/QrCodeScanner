be970d486f3cf6a9d0094db285ee7b83e1609d76
Alok Rathava <alokrathava@gmail.com>
2020-12-29 11:06:15 +0530

    Initial commit

---
+++ app/src/main/java/com/example/qrcodescanner/MainActivity.java
@@ -0,0 +1,38 @@
+ package com.example.qrcodescanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.qrcodescanner.databinding.ActivityMainBinding;
+
+ public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    public static TextView outtext;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        outtext = (TextView)findViewById(R.id.Outputtext);
+
+
+        binding.Scanbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),ScanCodeActivity.class));
+            }
+        });
+
+
+
+    }
+}
